d4fe47ed377e84582c35e077f5c946aa419b204a
aloncrack7 <alonmadrid@gmail.com>
2023-10-29 01:13:28 +0200

    Add: Times and optimization

--- webCompile.sh
+++ webCompile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-em++ fib/fib.cpp -o fib.js -s NO_EXIT_RUNTIME=1 -s EXPORTED_RUNTIME_METHODS=cwrap
+em++ fib/fib.cpp -o fib.js -s NO_EXIT_RUNTIME=1 -s EXPORTED_RUNTIME_METHODS=cwrap -O3
