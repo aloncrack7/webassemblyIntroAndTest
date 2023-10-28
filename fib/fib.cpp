37642c8ec797504130543019eac86545f2f34b5b
aloncrack7 <alonmadrid@gmail.com>
2023-10-28 13:28:02 +0200

    Fix: Fib basico

--- fib/fib.cpp
+++ fib/fib.cpp
@@ -1,22 +1,17 @@
 #include <iostream>
 #include <map>
+#include <emscripten.h>
 
 using namespace std;
 
-map<unsigned int, unsigned long> fibs;
+extern "C"{
+    EMSCRIPTEN_KEEPALIVE
+    unsigned long fib(unsigned int num){
+        if(num<2){
+            return num;
+        }
 
-unsigned long fib(unsigned int num){
-    if(num<2){
-        return num;
-    }
-
-    try{
-        return fibs.at(num);
-    }catch(const std::exception& e){
-        unsigned long result=fib(num-1)+fib(num-2);
-        fibs[num]=result;
-
-        return result;
+        return fib(num-1)+fib(num-2);
     }
 }
 
